b4126343c540484ff60c1640a30015f7034b287d
wishu <wish@sek.ai>
2022-01-01 15:38:22 +1100

    add regex filtering on message post

--- api.py
+++ api.py
@@ -10,6 +10,8 @@ setup_files('.auth_keys')
 with open('.auth_keys', 'r+') as fp:
     auth_keys = {line.rstrip() for line in fp}
 
+channel_whitelist = {'Star Miners'}
+
 app = falcon.asgi.App()
 
 class Message:
@@ -61,14 +63,50 @@ class Messages:
                 res.status = falcon.HTTP_200
                 content = json.loads(await req.stream.read())
                 for msg in content:
-                    if msg['chatType'] == 'FRIENDS' and msg['chatName'] == 'Star Miners':
+                    if msg['chatType'] == 'FRIENDS' and msg['chatName'] in channel_whitelist:
                         timestamp = int(datetime.datetime.strptime(msg['timestamp'][:19], '%Y-%m-%dT%H:%M:%S').timestamp())
-                        # regex match world, tier and location separately
-                        # if all 3 contain at least one match, generate new message
+                        body = msg['message']
+
+                        # Thanks to PescalinPax/MordoJay/Mordecaii for RegEx help
+                        world_pattern = re.compile('(?:world|w)?\s*(\d{3})')
+                        world_match = world_pattern.search(body)
+                        tier_pattern = re.compile('(?:tier|t)?\s*(\d{1})\s*(\d{1,3}\%)?')
+                        tier_match = tier_pattern.search(body)
+                        body = body.replace(world_match.group(0).strip(), '').replace(tier_match.group(0).strip(), '')
+                        location_pattern = re.compile('([a-z]{2}.*)')
+                        location_match = location_pattern.search(body)
+
+                        if world_match == None or tier_match == None or location_match == None:
+                            res.status = falcon.HTTP_400
+                            response = {'error': 'not valid scout data'}
+                            res.text = json.dumps(response)
+                            return
+
+                        # Handle optional percentage tracking
+                        if tier_match.group(2):
+                            tier = f'{tier_match.group(1)} {tier_match.group(2)}'
+                        else:
+                            tier = tier_match.group(1)
+
+                        message_entry = Message(
+                            msg['id'],
+                            timestamp,
+                            msg['sender'],
+                            world_match.group(1),
+                            tier,
+                            location_match.group(0)
+                        )
+
+                        """
+                        print(message_entry.world)
+                        print(message_entry.tier)
+                        print(message_entry.location)
+                        """
+
                         # check new message against star presences
                         # within algorithm, create OR update star presences
                     else:
-                        response = {'error': 'invalid message'}
+                        response = {'error': 'chat channel not in whitelist'}
                         res.text = json.dumps(response)
             else:
                 res.status = falcon.HTTP_401
